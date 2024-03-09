1914948ecf029c6165aa3de0ee6883689b290daa
Berkay <bagircan@etu.edu.tr>
2024-03-09 11:01:19 +0300

    Hey im committin

---
+++ src/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const path = require('path');
+const axios = require('axios');
+
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.get('/api/data', async (req, res) => {
+  try {
+    const apiResponse = await axios.get('https://api.adsb.one/v2/type/A321');
+    const data = apiResponse.data;
+
+    res.json(data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error fetching data');
+  }
+});
+
+app.use(express.static(path.join(__dirname, '../front-end/dist/front-end')));
+
+
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
